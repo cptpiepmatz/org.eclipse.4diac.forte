40ef763a86070a6e901395b123c31b375337c70d
Hesse, Tim <tim.hesse@uni-oldenburg.de>
2023-03-23 20:04:45 +0100

    fixed convert dds2ciec for uuid

--- src/com/dds/util/convert.cpp
+++ src/com/dds/util/convert.cpp
@@ -1,7 +1,5 @@
 #include "convert.h"
 
-#include <boost/format.hpp>
-
 namespace unique_identifier_msgs {
 namespace msg {
 
@@ -18,11 +16,30 @@ UUID ciec2dds(CIEC_ROS2_unique_identifier_msgs__msg__UUID paCIEC) {
 CIEC_ROS2_unique_identifier_msgs__msg__UUID dds2ciec(UUID paDDS) {
   unique_identifier_msgs::msg::uint8__16 uuid = paDDS.uuid();
 
-  boost::format fmt("(uuid:[%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d])");
-  for (const auto& value : uuid) fmt % value;
+  char fmt[100];
+  sprintf(
+    fmt, 
+    "(uuid:=[%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d])",
+    uuid[0],
+    uuid[1],
+    uuid[2],
+    uuid[3],
+    uuid[4],
+    uuid[5],
+    uuid[6],
+    uuid[7],
+    uuid[8],
+    uuid[9],
+    uuid[10],
+    uuid[11],
+    uuid[12],
+    uuid[13],
+    uuid[14],
+    uuid[15]
+  );
 
   CIEC_ROS2_unique_identifier_msgs__msg__UUID ciec;
-  ciec.fromString(fmt.str().c_str());
+  ciec.fromString(fmt);
 
   return ciec;
 };
